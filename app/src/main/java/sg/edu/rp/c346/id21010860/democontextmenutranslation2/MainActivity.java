eaac04c6b59c9617b83073d704d98d75b2824775
21010860 <21010860@myrp.edu.sg>
2022-06-01 23:48:10 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21010860/democontextmenutranslation2/MainActivity.java
@@ -0,0 +1,57 @@
+package sg.edu.rp.c346.id21010860.democontextmenutranslation2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText;
+    String wordClicked = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText.findViewById(R.id.textViewTranslatedText);
+        registerForContextMenu((tvTranslatedText));
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedText.setText("Hello");
+            return true;
+        }
+        else if (id == R.id.italianSelection) {
+            tvTranslatedText.setText("Ciao");
+            return true;
+        } else {
+            tvTranslatedText.setText("Error translation");
+        }
+        return super.onContextItemSelected(item);
+
+
+    }
+
+}
+
